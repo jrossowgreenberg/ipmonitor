074864a93b04bb023848da66939a8f95e26d5622
Julien Rossow-Greenberg <jrossowg@villanova.edu>
2025-01-11 10:31:11 -0500

    Bump version: 0.1.1 → 0.1.2

--- ipmonitor.py
+++ ipmonitor.py
@@ -7,7 +7,7 @@ from apprise import Apprise
 # ----------------------------
 # Configuration and Logging
 # ----------------------------
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
